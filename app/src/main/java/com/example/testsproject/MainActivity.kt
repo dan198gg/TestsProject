10eb8bbc2dec04231a3b43cdd5bc9ea80827ad36
dan198gg <danzelgoncharenko@gmail.com>
2024-07-16 09:31:57 +0300

    animationIsAdded

--- app/src/main/java/com/example/testsproject/MainActivity.kt
+++ app/src/main/java/com/example/testsproject/MainActivity.kt
@@ -1,16 +1,24 @@
 package com.example.testsproject
 
+import android.os.Build.VERSION.SDK_INT
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import coil.ImageLoader
+import coil.compose.rememberAsyncImagePainter
+import coil.decode.GifDecoder
+import coil.decode.ImageDecoderDecoder
 import com.example.testsproject.ui.theme.TestsProjectTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +26,20 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            TestsProjectTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            val imageLoader = ImageLoader.Builder(LocalContext.current)
+                .components {
+                    if (SDK_INT >= 28) {
+                        add(ImageDecoderDecoder.Factory())
+                    } else {
+                        add(GifDecoder.Factory())
+                    }
                 }
-            }
+                .build()
+            Image(
+                painter = rememberAsyncImagePainter(R.drawable.animationtests, imageLoader),
+                contentDescription = null,
+                modifier = Modifier.fillMaxSize()
+            )
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TestsProjectTheme {
-        Greeting("Android")
     }
 }
